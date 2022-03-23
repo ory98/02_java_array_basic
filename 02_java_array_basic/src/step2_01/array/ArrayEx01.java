5ee6581b5cb538ea7cd5910f5b19a9948b61e40a
15_web_jsb <15_web_jsb@DESKTOP-AH653HL>
2022-03-23 17:42:38 +0900

    3.23 학원 수업

--- 02_java_array_basic/src/step2_01/array/ArrayEx01.java
+++ 02_java_array_basic/src/step2_01/array/ArrayEx01.java
@@ -36,7 +36,61 @@ public class ArrayEx01 {
 
 	public static void main(String[] args) {
 		
-
+		// 1. 일반 변수 ( 값 저장 )
+		int nVar = 0;
+		nVar = 3;
+		System.out.println(nVar);
+		
+		System.out.println();
+		
+		// 2. 주소 변수 ( 주소 저장 )
+		// int[] = testArr = new int[5];
+		int[] arr = null; // 주소변수의 초깃값은 null(널=없음)로 지정한다. 
+		arr = new int[5]; 
+		
+		System.out.println(arr); // 배열의 변수명은 주소 
+		
+		System.out.println(arr[0]); // 배열은 초기값이 지정되어있다.
+		System.out.println(arr[1]);
+		System.out.println(arr[2]);
+		System.out.println(arr[3]);
+		System.out.println(arr[4]);
+		// System.out.println(arr[5]); 0부터 시작하며, 마지막이 5가 아니다.  *****중요***** 
+		// 배열의 최대범위를 넘어설때 발생하는 에러 > 3대 에러 중 하나 : java.lang.ArrayIndexOutOfBoundsException
+		
+		System.out.println();
+		
+		arr[0] = 10;
+		arr[1] = 20;
+		arr[2] = 30;
+		arr[3] = 40;
+		arr[4] = 50;		
+		
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+		System.out.println(arr[2]);
+		System.out.println(arr[3]);
+		System.out.println(arr[4]);
+		System.out.println();
+		
+		for (int i =0; i < arr.length; i++) { // 배열명.length :  배열의 크기를 반환한다.(알아서 측정해줌) 
+			System.out.println(arr[i]);
+		}
+		System.out.println();
+		
+		// 배열의 요소(element)를 삭제할 수 없다. (구성을 바꿀 수 없다 = 갯수를 바꿀 수 없다.)
+		// 초깃값으로 다시 리셋하여 데이터를 삭제한다. > 빈공간으로 두는 것이 속도가 더 빠르다. 
+		arr[3] = 0;
+		arr[4] = 0;
+		
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+		System.out.println();
+		
+		// null을 대입하여 배열자체를 삭제할 수 있다. 
+		arr = null;
+		
 	}
-
+	
 }
